b440fad1c3bfd53690060bb36ad33d0e07dbf158
peoplewhite <shonshon7@gmail.com>
2015-06-24 15:20:58 +0800

    add pods and AFNetworking done

---
+++ Podfile
@@ -0,0 +1,8 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'Demoapp_alphacamp' do
+pod "AFNetworking", "~> 2.0"
+end
+
+
---
+++ Pods/Target Support Files/Pods-Demoapp_alphacamp-AFNetworking/Pods-Demoapp_alphacamp-AFNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Demoapp_alphacamp_AFNetworking : NSObject
+@end
+@implementation PodsDummy_Pods_Demoapp_alphacamp_AFNetworking
+@end
