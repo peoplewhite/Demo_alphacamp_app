1b61b189395334c327282a36baaf34460dd671e6
peoplewhite <shonshon7@gmail.com>
2015-06-24 22:46:48 +0800

    add activityIndiactorView done

--- Demoapp_alphacamp/ViewController.m
+++ Demoapp_alphacamp/ViewController.m
@@ -18,11 +18,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-//    [[NSUserDefaults standardUserDefaults] setValue:nil forKey:@"thisUsername"];
-//    [[NSUserDefaults standardUserDefaults] setValue:nil forKey:@"thisPassword"];
-//    [[NSUserDefaults standardUserDefaults] synchronize];
+    [[NSUserDefaults standardUserDefaults] setValue:@"shonshon7@gmail.com" forKey:@"thisUsername"];
+    [[NSUserDefaults standardUserDefaults] setValue:@"qqpp1100" forKey:@"thisPassword"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
+
    
     if (([[NSUserDefaults standardUserDefaults] objectForKey:@"thisUsername"] != nil) && ([[NSUserDefaults standardUserDefaults] objectForKey:@"thisPassword"] != nil)) {
+        
+        UIActivityIndicatorView *indicatorView = [[UIActivityIndicatorView alloc]initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+        indicatorView.center = self.view.center;
+        [self.view addSubview:indicatorView];
+        [indicatorView startAnimating];
+        
+        
+        
+        
+        
         _btnLogin.hidden = 1;
         _manager = [AFHTTPRequestOperationManager manager];
         [_manager POST:@"https://dojo.alphacamp.co/api/v1/login"
@@ -31,8 +43,10 @@
                          @"password":[[NSUserDefaults standardUserDefaults] objectForKey:@"thisPassword"],
                          @"api_key":@"7c819379f329bc03ea4fcdb5f521831b5b920398"}
                success:^(AFHTTPRequestOperation *operation, id responseObject) {
+            [indicatorView stopAnimating];
             NSLog(@"yes");
         } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+            [indicatorView stopAnimating];
             NSLog(@"no");
         }];
     }
